aeee55cad4e76167cfb810b0e86f5cd8c115d644
Ian Rambo <ian@Ians-MBP.dhcp.lbl.gov>
2019-10-18 14:28:02 -0700

    cd-hit conda env

--- src/treebuild/conda_env.sh
+++ src/treebuild/conda_env.sh
@@ -4,4 +4,4 @@ envName=$1
 conda create -n $envName python=3.7 && \
 conda install -n $envName -c conda-forge scons && \
 conda install -n $envName -c anaconda numpy pandas && \
-conda install -n $envName -c bioconda iqtree
+conda install -n $envName -c bioconda iqtree cd-hit mafft
